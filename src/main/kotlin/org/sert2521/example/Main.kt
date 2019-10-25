c4e22e70444748b0d136b96f4f67c0b0f977158d
Aidan Heller <asfheller@gmail.com>
2019-10-24 23:44:50 -0700

    Initial commit

---
+++ src/main/kotlin/org/sert2521/example/Main.kt
@@ -0,0 +1,13 @@
+package org.sert2521.example
+
+import org.sert2521.sertain.events.onEnable
+import org.sert2521.sertain.robot
+
+// The robot function does all necessary work to set up the robot.
+fun main() = robot {
+    // onEnable sets up a listener for the Enable event. In other words,
+    // whenever the robot enables, this code will run.
+    onEnable {
+        println("Hello, world!")
+    }
+}
