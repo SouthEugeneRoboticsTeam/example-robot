eab5b7ad90da9ab528f2b7c8d5acfbd1709d7c50
Aidan Heller <asfheller@gmail.com>
2020-04-19 20:25:20 -0700

    Fix typo

--- src/main/kotlin/org/team0000/example/Main.kt
+++ src/main/kotlin/org/team0000/example/Main.kt
@@ -8,7 +8,7 @@ import org.sert2521.sertain.units.s
 import org.team0000.example.drivetrain.Drivetrain
 import org.team0000.example.drivetrain.driveForwardFor
 
-// Subsystems can be aquired and used through a `Worker`. These are made by calling `add`.
+// Subsystems can be acquired and used through a `Worker`. These are made by calling `add`.
 // Make sure to do this in your main file so it gets initialized right away.
 val drivetrain = add<Drivetrain>()
 
